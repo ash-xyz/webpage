5abeb5e6c92af0d3ee193f627b557673cb5e2af5
Ashraf Ali <ashrafali379@gmail.com>
2020-07-10 17:44:11 +0100

    Added Particles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!--<HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <!-- https://github.com/matteobruni/tsparticles -->
+    <Particles
+      id="tsparticles"
+      color="#dedede"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="true"
+      :linesOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    ></Particles>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+//import HelloWorld from "@/components/HelloWorld.vue";
+import Particles from "particles.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    //HelloWorld,
+    Particles
   }
 };
 </script>

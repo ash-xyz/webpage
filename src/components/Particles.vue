57ebaa135b2de716f33e1dbdeb3b6770c656f742
Ashraf Ali <ashrafali379@gmail.com>
2020-07-18 14:42:29 +0100

    Changed Particles to be a component

---
+++ src/components/Particles.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- https://github.com/matteobruni/tsparticles -->
+  <!-- Could try limegreen for the particle/line colour? 00FF00-->
+  <Particles
+    id="tsparticles"
+    color="#dedede"
+    :particleOpacity="0.7"
+    :particlesNumber="80"
+    shapeType="circle"
+    :particleSize="6"
+    linesColor="#dedede"
+    :linesWidth="2.5"
+    :lineLinked="true"
+    :linesOpacity="0.4"
+    :linesDistance="150"
+    :moveSpeed="3"
+    :hoverEffect="true"
+    hoverMode="grab"
+    :clickEffect="false"
+    class="change"
+  ></Particles>
+</template>
+
+<script>
+// @ is an alias to /src
+import Particles from "particles.vue";
+export default {
+  name: "particles",
+  components: { Particles }
+};
+</script>
+<style>
+.change {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: #1e1e1e;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,15 @@
 <template>
   <div class="home">
-    <!-- https://github.com/matteobruni/tsparticles -->
-    <!-- Could try limegreen for the particle/line colour? 00FF00-->
-    <Particles
-      id="tsparticles"
-      color="#dedede"
-      :particleOpacity="0.7"
-      :particlesNumber="80"
-      shapeType="circle"
-      :particleSize="4"
-      linesColor="#dedede"
-      :linesWidth="1"
-      :lineLinked="true"
-      :linesOpacity="0.4"
-      :linesDistance="150"
-      :moveSpeed="3"
-      :hoverEffect="true"
-      hoverMode="grab"
-      :clickEffect="false"
-      clickMode="push"
-    ></Particles>
+    <Particles></Particles>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Particles from "particles.vue";
+import Particles from "@/components/Particles.vue";
 export default {
   name: "Home",
   components: {
-    //HelloWorld,
     Particles
   }
 };

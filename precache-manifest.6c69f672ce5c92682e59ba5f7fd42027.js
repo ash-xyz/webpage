20a6b26f4776f852b415ec995795e74e86d47ab2
Ashraf Ali <ashrafali379@gmail.com>
2020-07-29 16:44:03 +0100

    Updates

---
+++ precache-manifest.6c69f672ce5c92682e59ba5f7fd42027.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cffd9d2de4dca5f9410c92466882a5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c049023f09222604ddc6",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "c65bce37d89458013d30",
+    "url": "/static/css/main.149565b2.chunk.css"
+  },
+  {
+    "revision": "c049023f09222604ddc6",
+    "url": "/static/js/2.3be563bc.chunk.js"
+  },
+  {
+    "revision": "a3c2e96374ee289f62d267dad476d288",
+    "url": "/static/js/2.3be563bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65bce37d89458013d30",
+    "url": "/static/js/main.0ca86f1a.chunk.js"
+  },
+  {
+    "revision": "98ff4f7da372f47cdd48",
+    "url": "/static/js/runtime-main.193bbaab.js"
+  },
+  {
+    "revision": "b4511becc9c77d70c9c0d2b8feb2fa0d",
+    "url": "/static/media/logo.b4511bec.svg"
+  },
+  {
+    "revision": "e33d093bc02a210fd328c9d7603fd9a9",
+    "url": "/static/media/logo.e33d093b.png"
+  },
+  {
+    "revision": "ec429af2dadf664592bff407435dce65",
+    "url": "/static/media/me.ec429af2.png"
+  }
+]);

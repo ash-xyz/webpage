33b9e41a5d0c30793d3300e008a3ae2ad6895ba6
Ash <=>
2021-12-24 01:30:05 +0000

    Created new Next JS and Tailwind Project

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div className="transform hover:scale-[1.01] transition-all rounded-xl w-full md:w-1/3 bg-gradient-to-r p-1 from-green-400 to-blue-500">
+             <div className="flex flex-col justify-between h-full bg-white dark:bg-gray-900 rounded-lg p-4">
+            <p className="font-extrabold text-transparent text-2xl bg-clip-text bg-gradient-to-b from-pink-400 to-red-500">
+                Website incoming
+            </p>
+            </div>
+        </div>
+    )
+  }

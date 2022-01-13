b5914560320fd3d158793c0f66003c468079e4d8
Ash Ali <ashrafali379@gmail.com>
2022-01-13 03:09:44 +0000

    Added Social Media

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,13 +17,13 @@ export default function Footer() {
             <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
             <div className="w-full border-gray-200 text-center">
                 <ExternalLink href="mailto:ashraf.ali@ucdconnect.ie">ashraf.ali@ucdconnect.ie</ExternalLink>
-                <p className="leading-normal my-5 text-gray-600 dark:text-gray-400">
+                <p className="leading-normal my-5 text-gray-500 dark:text-gray-500">
                     Mullingar,
                     <br />
                     WestMeath, Ireland
                 </p>
                 <span className="inline-flex">
-                    <a className="text-gray-500">
+                    <a className="text-gray-500 hover:text-gray-300" href="https://www.linkedin.com/in/ash-xyz/">
                         <svg
                             fill="currentColor"
                             stroke-linecap="round"
@@ -32,10 +32,10 @@ export default function Footer() {
                             className="w-5 h-5"
                             viewBox="0 0 24 24"
                         >
-                            <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
                         </svg>
                     </a>
-                    <a className="ml-4 text-gray-500">
+                    <a className="ml-4 text-gray-500 hover:text-gray-300" href="https://twitter.com/theashrafali">
                         <svg
                             fill="currentColor"
                             stroke-linecap="round"
@@ -47,7 +47,7 @@ export default function Footer() {
                             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
                     </a>
-                    <a className="ml-4 text-gray-500">
+                    <a className="ml-4 text-gray-500 hover:text-gray-300" href="https://www.instagram.com/notashraf/">
                         <svg
                             fill="none"
                             stroke="currentColor"
@@ -68,7 +68,7 @@ export default function Footer() {
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                         </svg>
                     </a>
-                    <a className="ml-4 text-gray-500">
+                    <a className="ml-4 text-gray-500 hover:text-gray-300">
                         <svg
                             fill="currentColor"
                             stroke-linecap="round"

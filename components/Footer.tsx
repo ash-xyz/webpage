fed271d347bf1ff53d9467cc556f5215406e1054
Ash Ali <ashrafali379@gmail.com>
2022-01-13 03:10:19 +0000

    Removed Messenger Icon

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -68,18 +68,6 @@ export default function Footer() {
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                         </svg>
                     </a>
-                    <a className="ml-4 text-gray-500 hover:text-gray-300">
-                        <svg
-                            fill="currentColor"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            stroke-width="2"
-                            className="w-5 h-5"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                        </svg>
-                    </a>
                 </span>
             </div>
         </footer >

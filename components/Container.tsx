3451bd29c55aca59b1c6b7f2a175110894115f38
Ash Ali <ashrafali379@gmail.com>
2022-01-17 22:39:44 +0000

    Added Headers

--- components/Container.tsx
+++ components/Container.tsx
@@ -33,10 +33,32 @@ export default function Container(props: any) {
 
     useEffect(() => setMounted(true), []);
 
-    const { children } = props;
+    const { children, ...customMeta } = props;
+    const router = useRouter();
+    const meta = {
+        title: 'Ash Ali',
+        description: `UCD Computer Science Student.`,
+        type: 'website',
+        ...customMeta
+    };
 
     return (
         <div className="bg-gray-50 dark:bg-gray-900">
+            <Head>
+                <title>{meta.title}</title>
+                <meta name="robots" content="follow, index" />
+                <meta content={meta.description} name="description" />
+                <meta property="og:url" content={`https://ash.xyz${router.asPath}`} />
+                <link rel="canonical" href={`https://ash.xyz${router.asPath}`} />
+                <meta property="og:type" content={meta.type} />
+                <meta property="og:site_name" content="Ash Ali" />
+                <meta property="og:description" content={meta.description} />
+                <meta property="og:title" content={meta.title} />
+                <meta property="og:image" content={meta.image} />
+                {meta.date && (
+                    <meta property="article:published_time" content={meta.date} />
+                )}
+            </Head>
             <div className="flex flex-col justify-center px-8">
                 <nav className="flex items-center justify-between w-full relative max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pt-8 pb-8 sm:pb-16  text-gray-900 bg-gray-50  dark:bg-gray-900 bg-opacity-60 dark:text-gray-100">
                     <a href="#skip" className="skip-nav">
